a968d67314d8f5fdc27debe6bd162d528ed716d0
J. Luis Arredondo <jose.arredondo.a@outlook.com>
2022-08-08 22:44:10 -0500

    Add 'react' routes

---
+++ app/javascript/components/App.js
@@ -0,0 +1,21 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import HelloWorld from "./HelloWorld"
+
+class App extends React.Component {
+  render () {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={"Home!"} />
+          <Route path="/hello" element={<HelloWorld greeting="Friend"/>} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App
