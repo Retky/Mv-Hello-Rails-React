d0a52f3d290adec764d2db7a733e7c30de32b51a
J. Luis Arredondo <jose.arredondo.a@outlook.com>
2022-08-09 04:57:23 -0500

    Add 'eslint' linters & fix

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,6 +1,6 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { fetchGreeting } from "../redux/reducers/greetings";
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchGreeting } from '../redux/reducers/greetings';
 
 const Greeting = () => {
   const dispatch = useDispatch();
@@ -8,8 +8,8 @@ const Greeting = () => {
 
   useEffect(() => {
     dispatch(fetchGreeting());
-  } , []);
+  }, []);
   return (<h1>{greet.message}</h1>);
-}
+};
 
 export default Greeting;
--- app/javascript/redux/configureStore.js
+++ app/javascript/redux/configureStore.js
@@ -1,12 +1,12 @@
-import { configureStore, } from '@reduxjs/toolkit';
-import { applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from "redux-thunk";
+import { configureStore } from '@reduxjs/toolkit';
+import { applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
 import greetingReducer from './reducers/greetings';
 
 const store = configureStore(
-  { reducer: { greeting: greetingReducer, }},
+  { reducer: { greeting: greetingReducer } },
   composeWithDevTools(applyMiddleware(thunk)),
 );
 
